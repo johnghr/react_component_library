a9b507741fc0508310af1a6d58c595c2d780aa28
John <johnghross@outlook.com>
2024-04-12 19:57:44 +0100

    added styles for disabled state

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,8 @@ const config: StorybookConfig = {
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@chromatic-com/storybook',
-        '@storybook/addon-interactions'
+        '@storybook/addon-interactions',
+        'storybook-addon-pseudo-states'
     ],
     framework: {
         name: '@storybook/react-vite',
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,13 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 import { Button } from '../components/Button/Button';
-import { withKnobs } from '@storybook/addon-knobs';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
     title: 'Button',
     component: Button,
-    decorators: [withKnobs],
     parameters: {
         // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
         layout: 'centered'
@@ -16,8 +14,8 @@ const meta = {
     tags: ['autodocs'],
     // More on argTypes: https://storybook.js.org/docs/api/argtypes
     argTypes: {
-        color: { control: 'color' },
-        size: { control: 'string' }
+        color: { control: 'radio', options: ['primary', 'secondary', 'tertiary'] },
+        size: { control: 'radio', options: ['small', 'medium', 'large'] }
     },
     // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
     args: { onClick: fn() }
@@ -30,20 +28,21 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
     args: {
         color: 'primary',
+        disabled: false,
         text: 'Button',
         type: 'button',
         variant: 'filled'
     }
 };
 
-export const Secondary: Story = {
-    args: {
-        color: 'secondary',
-        text: 'Button',
-        type: 'button',
-        variant: 'filled'
-    }
-};
+// export const Secondary: Story = {
+//     args: {
+//         color: 'secondary',
+//         text: 'Button',
+//         type: 'button',
+//         variant: 'filled'
+//     }
+// };
 // export const Large: Story = {
 //     args: {
 //         size: 'large',
